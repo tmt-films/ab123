cb55bca13b7d8c20bd678bef4aa1788901595d58
tmt-films <amxrrical@gmail.com>
2025-06-25 05:18:31 +0530

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -5,15 +5,15 @@ load_dotenv("./.env")
 
 
 class Config:
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "652g")
-  BOT_NAME = os.environ.get("BOT_NAME", "erewew")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "7917525595:AAHBFYfJnHEKTWUEXTHjGWlm5yfDAztx83k")
+  BOT_NAME = os.environ.get("BOT_NAME", "mrina")
 
-  API_ID = int(os.environ.get("API_ID", "12799559"))
-  API_HASH = os.environ.get("API_HASH", "077254e69d93d08357f25bb5f4504580")
+  API_ID = int(os.environ.get("API_ID", "25120562"))
+  API_HASH = os.environ.get("API_HASH", "0bd8eb78385a059f64f6032ebefc4615")
 
-  DATABASE_URI = os.environ.get("DATABASE_URI", "mongrites=true&w=majority")
-  SESSION_NAME = os.environ.get("DATABASE_NAME", "Aa")
+  DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://mrina:mrina@cluster0.xy5zmcd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  SESSION_NAME = os.environ.get("DATABASE_NAME", "mrina")
 
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1002049466903))
-  SUDO_USERS = [int(user) for user in (os.environ.get("SUDO_USERS","1137799257")).split()]
-  SUPPORT_CHAT_URL = os.environ.get("SUPPORT_CHAT_URL", "https://t.me/subotsupport")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1002522178088))
+  SUDO_USERS = [int(user) for user in (os.environ.get("SUDO_USERS","7769775189")).split()]
+  SUPPORT_CHAT_URL = os.environ.get("SUPPORT_CHAT_URL", "https://t.me/Queens_Supports")
